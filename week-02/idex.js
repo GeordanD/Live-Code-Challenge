18205626701691dd9716651d8048f9edd2001a42
Geordan Daugherty <geordan_daugherty@hotmail.com>
2022-10-20 18:01:18 -0500

    live coding update.

---
+++ week-02/idex.js
@@ -0,0 +1,35 @@
+// let bill = 1000;
+
+// console.log("Bill Amount: ", bill);
+
+// if(bill >= 1 && bill <= 100) {
+//     console.log("Tip Amount: ", bill * .22);
+// } else if(bill >= 101){
+//     console.log("Tip Amount ", bill * .20);
+// } else if(bill < 1 || bill == NaN) {
+//     console.log("you do not need to tip on 0 dollar bill", bill);
+// } 
+// else{
+//     console.log("Error, Please provide a number", bill);
+// }
+
+
+
+
+
+// let temperature = Math.random() * 100;
+// console.log("Current Temperature: ", temperature);
+
+// if(temperature < 60){
+//     console.log("It is cold outside, so I will wear some layers! temp: ", temperature.toFixed(2));
+// } else if(temperature > 60){
+//     console.log("It is warm outside, so I will wear a tank top! temp: ", temperature);
+// } else{
+//     console.log("cannot determine temperature");
+// }
+
+
+
+// for(let i = 0; i <= 10; i++) {
+//     console.log("Index", i);
+// }
